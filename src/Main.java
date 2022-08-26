8da1d039dfd8eccfe96e9ed06132bdbf39a4fff0
Sergey Kim <kimsenyuer@inbox.ru>
2022-08-27 02:17:53 +0500

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 4;
-        int b = 5;
-        int c = 6;
-        System.out.println(a + b + c);
-        System.out.println("Hello");
+        Numbers numbers = new Numbers(3,4,6);
+        System.out.println(numbers.getA() + numbers.getB() + numbers.getC());
+
+
     }
 }
